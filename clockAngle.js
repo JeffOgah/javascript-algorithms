060f0b2090538719488f9671a8b74d13d30b5ada
JeffOgah <31674136+JeffOgah@users.noreply.github.com>
2019-06-08 21:09:32 +0100

    Update clockAngle.js
    Refactored

--- clockAngle.js
+++ clockAngle.js
@@ -3,20 +3,16 @@ let minuteAngle, hourAngle, timeAngle;
 function clockAngle(time) {
   let timeStr = String(time); 
   let timeHr = Number(timeStr.substring(0,1)); //extract hour
-  let timeMin = Number(timeStr.substring(2,4)) //extract min
+  let timeMin = Number(timeStr.substring(2,4)); //extract min
 
-  if (timeMin > 0) {
-    //angle between two minutes = 6 degrees
-    minuteAngle = timeMin * 6; //to get angle from 12
+  //angle between two minutes = 6 degrees
+  minuteAngle = timeMin * 6; //to get angle from 12
+
+  //angle between 2 hours = 30 degrees
+  //get hour angle from 12 and account for extra degree according to minute
+  hourAngle = (timeHr * 30) + (timeMin/60 * 30);
+
+  timeAngle = Math.abs(minuteAngle - hourAngle);
   
-    //angle between 2 hours = 30 degrees
-    hourAngle = (timeHr * 30) + (timeMin/60 * 30); //get angle since hour hand is between 2 numbers
-  
-    timeAngle = Math.abs(minuteAngle - hourAngle);
-  }
-  else {
-    timeAngle = hourAngle = timeHr * 30;
-  }
- 
   return timeAngle;
 }
